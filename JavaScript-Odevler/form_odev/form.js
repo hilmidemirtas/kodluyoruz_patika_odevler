248c0408223b06675881028b732d983c21d1ada6
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-09 03:46:01 +0300

    Create form.js

---
+++ JavaScript-Odevler/form_odev/form.js
@@ -0,0 +1,8 @@
+   function form () {
+            let name = document.getElementById("isim").value;
+            let surname = document.getElementById("soyisim").value;
+            let phone = document.getElementById("tel").value;
+            let adress = document.getElementById("adres").value;
+
+            alert(name + " " + surname + " " + phone + " " + adress);
+        }
