2954ec56e45d1a51f3afaeec7e73018e1a007789
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-03-13 23:29:35 +0300

    Update Odev1.php
    code

--- PHP_Odevler/Odev1.php
+++ PHP_Odevler/Odev1.php
@@ -20,3 +20,24 @@ O O O O O O O O O O O O O O
 O O O O O O O O O O O O O O O
 O O O O O O O O O O O O O O O O   */
 
+<?php
+
+function triangle($value)
+{
+    if ($value >= 1) {
+        $counter = 1;
+        $result = "";
+        while ($counter <= $value) {
+            for ($i = 1; $i <= $counter; $i++) {
+                $result .= 0;
+            }
+            $result .= "<br>";
+            $counter++;
+        }
+    } else {
+        $result = "Please enter a number of 1 or greater.";
+    }
+    return $result;
+}
+
+echo triangle(15);
